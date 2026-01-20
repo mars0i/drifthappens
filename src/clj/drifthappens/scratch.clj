e785369ecc328ea8a319c18fe15e4a8fd4f978f0
Marshall Abrams <mabrams001@bham.rr.com>
2026-01-20 16:53:15 -0600

    Experiments with fastmath matrices

---
+++ src/clj/drifthappens/scratch.clj
@@ -0,0 +1,50 @@
+;; Initial ns statement copied from
+;; https://generateme.github.io/fastmath/clay/vector_matrix.html#matrices
+(ns drifthappens.scratch
+  (:require [fastmath.vector :as v]
+            [fastmath.matrix :as mat]
+            ;[fastmath.dev.codox :as codox]
+            ;[fastmath.dev.clay :as utls]
+            ;[fastmath.dev.plotly :as plt]
+            [fastmath.core :as m]
+            [scicloj.kindly.v4.kind :as kind]
+            ;[fastmath.dev.ggplot :as gg]
+            [fastmath.stats :as stats]
+            )
+  (:import [fastmath.vector Vec2 Vec3 Vec4]
+           [fastmath.matrix Mat2x2 Mat3x3 Mat4x4]))
+
+
+(def mat1 (mat/real-matrix [[1 2 3 4]
+                            [5 6 7 8]]))
+
+(def mat2 (mat/mat         [[1 2 3 4]
+                            [5 6 7 8]]))
+
+(def mat3 (mat/mat         [[1 2]
+                            [5 6]]))
+(type mat1)
+(type mat2)
+(type mat3)
+
+
+(= mat1 mat2)
+
+(mat/entry mat1 1 1)
+(mat/entry mat2 1 1)
+
+(mat/shape mat1)
+(mat/shape mat2)
+
+(def mm1 (mat/mulmt mat1 mat2))
+(mat/shape mm1)
+(mat/entry mm1 1 1)
+
+(def mm2 (mat/mulm mat1 (mat/transpose mat2)))
+(mat/shape mm2)
+(mat/entry mm2 1 1)
+
+(def mm3 (mat/mulm (mat/transpose mat1) mat2))
+(mat/shape mm3)
+(mat/entry mm3 1 1)
+(mat/entry mm3 3 3)
