cde5f7224bac265c665b8f6c437e3faab687f531
Marshall Abrams <mabrams001@bham.rr.com>
2026-01-27 15:11:00 -0600

    Exploration

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -9,78 +9,36 @@
             [scicloj.tableplot.v1.plotly :as plotly]
             [tablecloth.api :as tc]
             [drifthappens.wrightfisher :as wf]
-            [utils.plotly :as up]
-            [utils.math :as umath]
-            [utils.misc :as umisc])
+            [utils.misc :as misc])
   (:import [fastmath.vector Vec2 Vec3 Vec4]
            [fastmath.matrix Mat2x2 Mat3x3 Mat4x4]))
 
 
-;; ??: 
-;;  <groupId>org.apache.commons</groupId>
-;;     <artifactId>commons-math3</artifactId>
-;;     <version>3.6.1</version>
-;; </dependency>
-;; 
-;; Once set up, we can use the RealMatrix interface and its
-;; Array2DRowRealMatrix implementation to create our usual matrices.
-;; The constructor of the implementation class takes a
-;; two-dimensional double array as its parameter:
-;; 
-;; RealMatrix matrix = new Array2DRowRealMatrix(/* a two dimensions double array */);
-
-;; https://commons.apache.org/proper/commons-math/javadocs/api-3.6.1/org/apache/commons/math3/linear/Array2DRowRealMatrix.html
-;; https://commons.apache.org/proper/commons-math/javadocs/api-3.6.1/org/apache/commons/math3/linear/AbstractRealMatrix.html
+(defn plot-iterates
+  "Plot lines connecting n iterates as y values with x representing number of
+  iterations.  iterates should not be infinite."
+  [iterates]
+  (-> (tc/dataset {:x (range (count iterates))
+                   :y iterates})
+      ;(plotly/base {:=width 900})
+      (plotly/layer-line {:=x :x, :=y, :y})
+      plotly/plot
+      (assoc-in [:data 0 :line :width] 1)))
 
 
-(def init-freqs (wf/mkvec (concat (repeat 500 0.0) [1.0] (repeat 500 0.0))))
-(def upmat (wf/right-mult-tran-mat 0.95 1.0 (dec (count init-freqs))))
-;(type upmat)
+;; pop size 50 with 50% A, 50% B.
+(def init-freqs (wf/mkvec (concat (repeat 25 0.0) [1.0] (repeat 25 0.0))))
 
-(up/plot-lines init-freqs)
-(up/plot-lines (fmat/mulv (umath/mpow upmat 2) init-freqs))
-(up/plot-lines (fmat/mulv (umath/mpow upmat 4) init-freqs))
-(up/plot-lines (fmat/mulv (umath/mpow upmat 20) init-freqs))
-(up/plot-lines (fmat/mulv (umath/mpow upmat 40) init-freqs))
-;(up/plot-lines (fmat/mulv (umath/mpow upmat 80) init-freqs))
+(def upmat (wf/right-mult-tran-mat 0.7 0.5 50))
 
 (comment
-  (def upmat2 (umath/mpow upmat 2))
-  (def upmat4 (umath/mpow upmat2 2))
-  (def upmat16 (umath/mpow upmat4 2))
-  (def upmat256 (umath/mpow upmat16 2))
-  (def upmat65K (umath/mpow upmat256 2))
-  (up/plot-lines init-freqs)
-  (up/plot-lines (fmat/mulv upmat init-freqs))
-  (up/plot-lines (fmat/mulv upmat2 init-freqs))
-  (up/plot-lines (fmat/mulv upmat4 init-freqs))
-  (up/plot-lines (fmat/mulv upmat16 init-freqs))
-  (up/plot-lines (fmat/mulv upmat256 init-freqs))
-  (up/plot-lines (fmat/mulv upmat65K init-freqs))
+  (fmat/mulv upmat init-freqs)
+  (fmat/mulv (wf/mpow upmat 3) init-freqs)
+
+
 )
 
-(comment
-  (let [init-freqs (wf/mkvec (concat (repeat 50 0.0) [1.0] (repeat 50 0.0)))
-        upmat (wf/right-mult-tran-mat 1.0 1.0 (dec (count init-freqs)))]
-    (kind/fragment
-      [
-       ;(up/plot-dots init-freqs)
-       ;(up/plot-dots (fmat/mulv upmat init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 2) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 3) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 8) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 16) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 30) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 40) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 50) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 60) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 70) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 80) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 90) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 100) init-freqs))
-       (up/plot-dots (fmat/mulv (umath/mpow upmat 200) init-freqs))
-       ;(up/plot-dots (fmat/mulv (umath/mpow upmat 500) init-freqs))
-      ]
-    )
-  )
-)
+(plot-iterates init-freqs)
+(plot-iterates (fmat/mulv upmat init-freqs))
+(plot-iterates (fmat/mulv (wf/mpow upmat 3) init-freqs))
+(plot-iterates (fmat/mulv (wf/mpow upmat 8) init-freqs))
