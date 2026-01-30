a8c2a301f9441ecc5427a9010b3d0f9fdf4cf9bf
mabrams <mabrams001@bham.rr.com>
2026-01-30 15:20:03 -0600

    Increased large pop, decreased small, using new tran elems fn

--- src/clj/drifthappens/predreprod1.clj
+++ src/clj/drifthappens/predreprod1.clj
@@ -19,7 +19,7 @@
         tran-mats))
 
 (def fit-B 1.0)
-(def big-fit-A 1.001)  ; large size has sel
+(def big-fit-A 1.01)  ; large size has sel
 (def small-fit-A 1.0) ; small size is pure drift
 (def increments (iterate (partial + 16) 0))
 (def generations (map inc increments))
@@ -32,84 +32,11 @@
 )
 
 
-(def num-gens 20)
+(def num-gens 15)
 
-;; should be even;
-(def small-N 20)
-(def big-N 500)
-
-(def half-small-N (/ small-N 2))
-(def half-big-N (/ big-N 2))
-
-(def small-pop-init (wf/mkvec (concat (repeat half-small-N 0.0) [1.0] (repeat half-small-N 0.0))))
-(def small-drift-mat (wf/right-mult-tran-mat small-fit-A fit-B (dec (count small-pop-init))))
-(def small-tran-mats (umath/make-mat-powers small-drift-mat (take num-gens generations)))
-(def small-prob-states (make-prob-states small-tran-mats small-pop-init))
-(def small-plots (mapv uplot/plot-both small-prob-states))
-
-(def big-pop-init (wf/mkvec (concat (repeat half-big-N 0.0) [1.0] (repeat half-big-N 0.0))))
-(def big-drift-mat (wf/right-mult-tran-mat big-fit-A fit-B (dec (count big-pop-init)))) ; use fit-B for fit-A to make them equal
-(def big-tran-mats (umath/make-mat-powers big-drift-mat (take num-gens generations)))
-(def big-prob-states (make-prob-states big-tran-mats big-pop-init))
-(def big-plots (mapv uplot/plot-lines big-prob-states))
-
-;small-plots
-;big-plots
-
-(def N (dec (count big-pop-init)))
-(def M (dec (count small-pop-init)))
-
-;; Shrinking and expanding matrices.
-;; Note M and N are swapped in the next two
-(def predat-drift-mat (wf/right-mult-tran-mat small-fit-A fit-B N M)) ; 
-(def reprod-drift-mat (wf/right-mult-tran-mat big-fit-A fit-B M N)) ; use fit-B for fit-A to make them equal
-;; Combine into one square matrix:
-(def pred-reprod-mat (fmat/mulm reprod-drift-mat predat-drift-mat))
-
-;; We use half-generations here because each step involves two sampling
-;; processes.  So each generation is analogous to two generations in the
-;; small and big models.
-(def pred-reprod-tran-mats (umath/make-mat-powers pred-reprod-mat (take num-gens half-generations)))
-(def pred-reprod-prob-states (make-prob-states pred-reprod-tran-mats big-pop-init))
-(def pred-reprod-plots (mapv uplot/plot-both pred-reprod-prob-states))
-
-;pred-reprod-plots
-
-(def small-big-combo-plots (interleave small-plots big-plots pred-reprod-plots))
-
-small-big-combo-plots 
-
-
-(comment
-  (fmat/shape predat-drift-mat)
-  (fmat/shape reprod-drift-mat)
-  (fmat/shape pred-reprod-mat)
-  (map fmat/shape pred-reprod-tran-mats)
-)
-
-(defn make-prob-states
-  [tran-mats init-state]
-  (mapv (fn [m] (fmat/mulv m init-state))
-        tran-mats))
-
-(def fit-B 1.0)
-(def big-fit-A 1.001)  ; large size has sel
-(def small-fit-A 1.0) ; small size is pure drift
-(def increments (iterate (partial + 16) 0))
-(def generations (map inc increments))
-(def half-generations (map (fn [n] (inc (/ n 2))) increments))
-(comment
-  (take 10 increments)
-  (take 10 generations)
-  (take 10 half-generations)
-)
-
-
-(def num-gens 20)
-
-;; should be even;
-(def small-N 30)
-(def big-N 1000)
+;; These numbers need to be even:
+(def small-N 10)
+(def big-N 2000)
 
 (def half-small-N (/ small-N 2))
 (def half-big-N (/ big-N 2))
@@ -159,4 +86,3 @@ small-big-combo-plots
   (fmat/shape pred-reprod-mat)
   (map fmat/shape pred-reprod-tran-mats)
 )
-
