4cd8850e18b96f9bdc05af962a31a86446a0a955
Marshall Abrams <mabrams001@bham.rr.com>
2026-02-01 13:20:21 -0600

    Tested, refined sequences of powers functions

--- src/clj/utils/math.clj
+++ src/clj/utils/math.clj
@@ -6,17 +6,20 @@
 ;; Fewer multiplications. Faster for larger m and n than simpler version.
 ;; Uses mutual recursion, but stack depth is O(log2 n).
 (defn mpow
-  "Multiplies a square matrix by itself n times.  n must be a positive
-  integer or positive floating-point number with no fractional part."
+  "Multiplies a square matrix by itself n times.  n must be a nonnegative
+  integer or positive floating-point number with no fractional part. Returns
+  the identity nmatrix of the same shape when n=0."
   [m n]
   (let [[h w] (fmat/shape m)]
     (cond 
       (not= h w) 
       (print (str "mpow only multiplies square matrices. Matrix shape was [" h " " w "].")) ; throw?
 
-      (or (<= n 0)
-          (not (zero? (rem n 1))))
-      (print (str "mpow only accepts positive integer powers. Exponent was " n)) ; throw?
+      (or (< n 0) (not (zero? (rem n 1))))
+      (print (str "mpow only accepts nonnegative integer powers. Exponent was " n)) ; throw?
+
+      (zero? n)
+      (fmat/mat->array2d (fmat/eye h)) ; at present need to convert eye to Java Array2D for mult with Array2D
 
       :else 
       (letfn [(either-pow [acc-mat k]
@@ -28,73 +31,47 @@
                   (fmat/mulm factor factor)))]
         (either-pow m (long n)))))) ; allow float integers
 
-(comment
-  ;; Simple, tail recursive, but slow for moderately large m and n.
-  (defn mpow-slow
-    "Multiplies a square matrix by itself n times.  n must be a positive
-    integer or positive floating-point number with no fractional part."
-    [m n]
-    (let [[h w] (fmat/shape m)]
-      (cond 
-        (not= h w)
-        (print (str "mpow only multiplies square matrices; shape is [" h " " w "].")) ; throw?
-
-        (or (<= n 0) (not (== (rem n 1) 0)))
-        (print "mpow only accepts positive integer powers.") ; throw?
-
-        :else 
-        (loop [i 1, acc-mat m]
-          (if (= i n)
-            acc-mat
-            (recur (inc i)
-                   (fmat/mulm m acc-mat)))))))
-)
-
-
-
-(defn make-mat-powers-separately
-  "Returns a sequence of transition matrices that are integer powers of of
-  square matrix m for each exponent in expts.  May be less efficient than
-  other methods if the indexes are close together."
-  [m expts]
-  (map (partial mpow m) expts))
-
-
 (defn mat-powers
   "Returns a lazy sequence of powers of a square matrix beginning with
   m^0, i.e. begining with the identity matrix of the same size.  (Specific
-  elements can be extracted e.g. using take-nth or utils/misc/elems-at.)"
+  elements can be extracted e.g. using take-nth, keep-indexed, or 
+  utils/misc/elems-at.)"
   [m]
   (iterate (partial fmat/mulm m)
            (fmat/mat->array2d ; at present need to convert eye to Java Array2D for mult with Array2D
              (fmat/eye (first (fmat/shape m))))))
 
-(defn make-mat-powers-sequentially
-  [m expts]
-  (let [powseq (mat-powers m)]
-    (loop [i 0, pows (mat-powers m), es expts, acc-pows []]
-      (if (empty? expts)
-        acc-pows
-        (if (= i (first es))
-          (recur (inc i)
-                 (rest es)
-                 (rest pows)
-                 (conj acc-pows (first pows)))
-          (recur (inc i)
-                 es ; don't move on until we find it
-                 (rest pows)
-                 acc-pows))))))
-
-;; Can I rewrite the preceding like this?
-;; This is lazy but doesn't allow infinite seqs since using set.
-;; Is that what I want?
-(defn make-mat-powers-sequentially2
+(defn choose-mat-powers-sequentially
+  "Returns a lazy sequence of transition matrices that are integer powers
+  of square matrix m for each exponent in expts (which may be in any
+  order). Exponents must be nonnegative integers, which may be in a float
+  representation."
   [m expts]
   (let [expts-set (set expts)
-        n (count expts-set)] ; allow dupes
-    (take n
-          (keep-indexed (fn [i m] (when (expts-set i) m))
-                        (mat-powers m)))))
+        n (count expts-set)] ; don't count dupes
+    (take n (keep-indexed (fn [i m] (when (expts-set i) m))
+                          (mat-powers m)))))
+
+;; FIXME floats are causing this to not return
+(defn choose-mat-powers-separately
+  "Returns a lazy sequence of transition matrices that are integer powers
+  of square matrix m for each exponent in expts (which may be in any
+  order). Exponents must be nonnegative integers, which may be in a float
+  representation."
+  [m expts]
+  (map (partial mpow m) (map long expts)))
+
+(comment
+  (def m (fm/seq->double-double-array [[1 2 3][4 5 6][7 8 9]]))
+  (def ms (mat-powers m))
+  (take 10 ms)
+  (nth ms 9)
+  (nth ms 10)
+  (def mpsep (choose-mat-powers-separately m [0 1 4 9]))
+  (def mpsep2 (choose-mat-powers-separately m [0.0 1.0 4.0 9.0]))
+  (def mpseq (choose-mat-powers-sequentially m [0 1 4 9]))
+  (def mpseq2 (choose-mat-powers-sequentially m [1.0 4.0 9])) ; loops forever why?
+)
 
 
 (comment
@@ -136,5 +113,50 @@
   (type I)
   (type Ireal)
   (type Idouble)
+)
+
+
+(comment
+;; There's a bug here that causes an infinite loop though the second
+;; clause of the second if.  I don't understand it, but another verison
+;; using `keep-indexed` works, so I'm not going to bother to fix this
+;; unless there's a good reason to resuscitate it.
+(defn make-mat-powers-sequentially-bad
+  [m expts]
+  (let [powseq (mat-powers m)]
+    (loop [i 0, pows (mat-powers m), es expts, acc-pows []]
+      (if (empty? expts)
+        acc-pows
+        (if (= i (first es))
+          (recur (inc i)
+                 (rest es)
+                 (rest pows)
+                (conj acc-pows (first pows)))
+          (recur (inc i)
+                 es ; don't move on until we find one
+                 (rest pows)
+                 acc-pows))))))
+)
+
+(comment
+  ;; Simple, tail recursive, but slow for moderately large m and n.
+  (defn mpow-slow
+    "Multiplies a square matrix by itself n times.  n must be a positive
+    integer or positive floating-point number with no fractional part."
+    [m n]
+    (let [[h w] (fmat/shape m)]
+      (cond 
+        (not= h w)
+        (print (str "mpow only multiplies square matrices; shape is [" h " " w "].")) ; throw?
+
+        (or (<= n 0) (not (== (rem n 1) 0)))
+        (print "mpow only accepts positive integer powers.") ; throw?
+
+        :else 
+        (loop [i 1, acc-mat m]
+          (if (= i n)
+            acc-mat
+            (recur (inc i)
+                   (fmat/mulm m acc-mat)))))))
 )
 
